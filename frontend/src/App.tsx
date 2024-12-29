83b3cc4fda2b214ab2b1bea17e32dd37004ccf4e
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2024-12-29 14:15:44 -0500

    Add Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 const App: React.FC = (): React.JSX.Element => {
   return (
     <>
-      <p>Hello RSA</p>
+      <header>
+        <Header />
+      </header>
+      <main>
+        
+      </main>
     </>
   );
 };
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header: React.FC = (): React.JSX.Element => {
+  return (
+    <>
+      <h1>RSA Encryption</h1>
+      <h4>Encode, decode, and break codes in a few clicks!</h4>
+      <p>
+        <i>
+          *For informational purposes only. Not meant to protect actual sensitive information.
+        </i>
+      </p>
+    </>
+  );
+};
+
+export default Header;
