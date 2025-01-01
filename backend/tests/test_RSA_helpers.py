e60f18071cef6d0be1fe206ef7cb0189a281c869
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-01-01 14:32:42 -0500

    Write tests for RSA helpers

---
+++ backend/tests/test_RSA_helpers.py
@@ -0,0 +1,15 @@
+import pytest
+from calculations.RSA_helpers import FME, euclidean_alg, EEA
+
+
+def test_FME():
+    assert FME(b=529, n=8, m=157) == 529 ** 8 % 157
+
+
+def test_euclidean_alg():
+    assert euclidean_alg(25, 135) == 5
+    assert euclidean_alg(19, 1829) == 1
+
+
+def test_EEA():
+    assert EEA(252, 198) == (18, (4, -5))
