467821aa8127171efe522eaad60bd0425c07d1e0
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-01-04 14:36:33 -0500

    Write test for WriteMessage

--- frontend/src/components/WriteMode/WriteMessage.test.tsx
+++ frontend/src/components/WriteMode/WriteMessage.test.tsx
@@ -1,1 +1,16 @@
-test('dummy test', () => expect(1).toBe(1));
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import WriteMessage from './WriteMessage';
+
+describe('WriteMessage', () => {
+  it('should show Encode only after the user has entered a public key', async () => {
+    render(<WriteMessage />);
+    const user = userEvent.setup();
+    expect(screen.queryByText(/Encode Message/)).not.toBeInTheDocument();
+    await user.type(screen.getByLabelText('n:'), '135');
+    await user.type(screen.getByLabelText('e:'), '246');
+    await user.click(screen.getByText('Submit'));
+    expect(screen.queryByText(/Encode Message/)).toBeInTheDocument();
+  });
+});
