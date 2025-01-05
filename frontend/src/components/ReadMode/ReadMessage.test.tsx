134e8874421d0d663188db22dee2f6d08b7a2f37
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2025-01-05 17:04:59 -0500

    Write test for ReadMessage

--- frontend/src/components/ReadMode/ReadMessage.test.tsx
+++ frontend/src/components/ReadMode/ReadMessage.test.tsx
@@ -1,1 +1,16 @@
-test('dummy test', () => expect(1).toBe(1));
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import ReadMessage from './ReadMessage';
+
+describe('ReadMessage', () => {
+  it('should show Decode only after the user has entered a private key', async () => {
+    render(<ReadMessage />);
+    const user = userEvent.setup();
+    expect(screen.queryByText(/Decode Message/)).not.toBeInTheDocument();
+    await user.type(screen.getByLabelText('n:'), '531');
+    await user.type(screen.getByLabelText('d:'), '642');
+    await user.click(screen.getByText('Submit'));
+    expect(screen.queryByText(/Decode Message/)).toBeInTheDocument();
+  });
+});
